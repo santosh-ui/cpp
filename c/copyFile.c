123adc6708c0924496aeb677cc823d6fb31d3d73
Sai Santosh Raju Prathigadapa <rajusantosh77@gmail.com>
2022-06-27 12:15:02 +0100

    EOF = -1 = CTRL+Z/CTRL+D

--- c/copyFile.c
+++ c/copyFile.c
@@ -1,7 +1,11 @@
-#include<stdio.h>
-void main() {
+#include <stdio.h> 
+ /* copy input to output; 1st version */ 
+ void main() 
+ { 
     int c; 
-    c =  getchar();
-    while (c != EOF) 
-    putchar(c); 
-}
+    // c = getchar(); 
+    while ((c = getchar()) != EOF) { // EOF = -1 = CTRL+Z / CTRL+D
+        // printf("The character entered is: %c \n", c);
+        putchar(c);
+    } 
+ }
