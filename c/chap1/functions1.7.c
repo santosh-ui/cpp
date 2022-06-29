806c112f6fe9d06476efc5a404acceecc6b81499
Sai Santosh Raju Prathigadapa <rajusantosh77@gmail.com>
2022-06-29 10:50:55 +0100

    old version style of function

--- c/chap1/functions1.7.c
+++ c/chap1/functions1.7.c
@@ -1,15 +1,28 @@
 #include<stdio.h>
 /* power: raise base to n-th power; n >= 0 */ 
-int power(int m, int n) {
-    int p = 1;
-    for(int i = 0; i < n; i++) {
-        p *= m;
-    }
-    return p;
-}
+int power(int m, int n);
+// {
+//     int p = 1;
+//     for(int i = 0; i < n; i++) {
+//         p *= m;
+//     }
+//     return p;
+// }
 
 void main() {
     for (int i = 0; i <= 10; i++) {
         printf("%d %d %d\n", i, power(2, i), power(-3, i));
     }
 }
+
+/* power: raise base to n-th power; n >= 0 */ 
+ /* (old-style version) */ 
+ int power(base, n) 
+ int base, n; 
+ { 
+ int i, p; 
+ p = 1; 
+ for (i = 1; i <= n; ++i) 
+ p = p * base; 
+ return p; 
+ } 
