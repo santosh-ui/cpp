abeabe66a487a3b8a6fe6f6747271338a5a18eb9
Sai Santosh Raju Prathigadapa <rajusantosh77@gmail.com>
2023-01-16 12:55:45 +0000

    exercises 3.2.2

--- cpp-primer/chapter2/strCreation.cc
+++ cpp-primer/chapter2/strCreation.cc
@@ -39,8 +39,8 @@ int main(void) {
     // size returns a (string::size_type) value.
     // we don't know the precise type but what we do know is
     // it is an unsigned type big enough to hold the size of any string
-    string line;
-    getline(cin, line);
+    // string line;
+    // getline(cin, line);
     // string::size_type len = line.size();
     // cout << len << endl;
 
@@ -48,8 +48,61 @@ int main(void) {
     // under the new standard, we ask the compiler to provide 
     // the appropriate type by usin (auto) or (decltype)
 
-    auto len = line.size();
-    cout << len << endl;
+    // auto len = line.size();
+    // cout << len << endl;
+
+    /*
+     * Tip
+     * You can avoid problems due to conversion between unsigned and int by
+     * not using ints in expressions that use size().
+     */
+
+    // Exercises 3.2.2
+    /*
+     * Exercise 3.4: Write a program to read two strings and report whether the
+     * strings are equal. If not, report which of the two is larger. Now, change
+     * the program to report whether the strings have the same length, and if
+     * not, report which is longer.
+     */
+
+    // string str1;
+    // string str2;
+    // cout << "Enter string 1: " << endl;
+    // getline(cin, str1);
+    // cout << "Enter string 2: " << endl;
+    // getline(cin, str2);
+
+    // if (str1 == str2) {
+    //     cout << "str1 and str2 are equal." << endl;
+    // } else if (str1 > str2) {
+    //     cout << "str1 is greater than str2." << endl;
+    // } else {
+    //     cout << "str2 is greater than str1." << endl;
+    // }
+
+    // if (str1.size() > str2.size()) {
+    //     cout << "str1 is longer than str2." << endl;
+    //     cout << "str1 size: " << str1.size() << endl;
+    //     cout << "str2 size: " << str2.size() << endl;
+    // } else {
+    //     cout << "str2 is longer than str1." << endl;
+    //     cout << "str1 size: " << str1.size() << endl;
+    //     cout << "str2 size: " << str2.size() << endl;
+    // }
+
+    /* 
+     * Exercise 3.5: Write a program to read strings from the standard input,
+     * concatenating what is read into one large string. Print the concatenated
+     * string. Next, change the program to separate adjacent input strings by a space.
+     */
+
+    string word;
+    string line;
+    while(cin >> word) { // read each word until EOF is encountered
+        line += word + " "; // Concatenated string seperated by a space
+    }
+
+    cout << "line: " << line << endl;
 
     return 0;
 }
