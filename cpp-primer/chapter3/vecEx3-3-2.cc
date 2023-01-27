a3d4c42f54bba45333a7703592771084c258b37a
Sai Santosh Raju Prathigadapa <rajusantosh77@gmail.com>
2023-01-27 10:07:59 +0000

    adds push_back exercises

--- cpp-primer/chapter3/vecEx3-3-2.cc
+++ cpp-primer/chapter3/vecEx3-3-2.cc
@@ -4,28 +4,34 @@
 using std::cin, std::cout, std::endl;
 using std::string, std::vector;
 int main(void) {
-    int n;
-    string s;
-    vector<int> vi;
-    vector<string> vs;
-    cout << "Enter the members of the int vector seperated by space: " << endl;
-    while (cin >> n) {
-        vi.push_back(n);
-    }
+    // int n;
+    // string s;
+    vector<int> vi{1, 2, 3, 4, 5};
+    for (int &i: vi) // for each element in vi
+        i *= i; // square each element value in the vector
+    
+    for (int i: vi) // for each integer in vi
+        cout << i << endl; // print the element value
 
-    cout << "vi elements are: " << endl;
-    for (int i: vi) {
-        cout << i << endl;
-    }
+    // vector<string> vs;
+    // cout << "Enter the members of the int vector seperated by space: " << endl;
+    // while (cin >> n) {
+    //     vi.push_back(n);
+    // }
 
-    cout << "Enter the elements of the string vector seperated by space: " << endl;
-    while (cin >> s) {
-        vs.push_back(s);
-    }
+    // cout << "vi elements are: " << endl;
+    // for (int i: vi) {
+    //     cout << i << endl;
+    // }
 
-    cout << "vs elements are: " << endl;
-    for (string word: vs)
-        cout << word << endl;
+    // cout << "Enter the elements of the string vector seperated by space: " << endl;
+    // while (cin >> s) {
+    //     vs.push_back(s);
+    // }
+
+    // cout << "vs elements are: " << endl;
+    // for (string word: vs)
+    //     cout << word << endl;
     
     return 0;
 }
